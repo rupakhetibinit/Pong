cebd1e7db46d23dfb17bec5efdb3b0633ae0cbd8
Binit Rupakheti <crusierponder@protonmail.com>
2023-04-02 23:13:38 +0545

    Added state for playing and paused

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -12,10 +12,35 @@ struct Ball {
 	}
 };
 
+struct Hitter {
+	float posX, posY;
+
+	float speed;
+	void Draw()
+	{
+		DrawRectangle(posX,posY, 10, 100, WHITE);
+
+	}
+	Rectangle getRect() {
+		return Rectangle{ posX, posY, 10, 100 };
+	}
+};
+
+enum State {
+	STARTED, PAUSED
+};
+
+enum Win {
+	LEFT, RIGHT,NONE
+};
+
 int main() 
 {
+	SetConfigFlags(FLAG_VSYNC_HINT);
 	InitWindow(1024, 768, "Ping Pong");
 	SetTargetFPS(60);
+	State state(STARTED);
+	Win win = NONE;
 	Ball ball;
 	ball.speedX = 5;
 	ball.speedY = 5;
@@ -24,34 +49,98 @@ int main()
 //	ball.posY = GetScreenHeight() / 2;
 	ball.posX = 0.0f;
 	ball.posY = 0.0f;
+	
+	Hitter leftHitter{};
+	Hitter rightHitter{};
+	leftHitter.posX = 50.0f;
+	leftHitter.posY = float(GetScreenHeight() / 2 - 50);
+	rightHitter.posX = float(GetScreenWidth() - 50);
+	rightHitter.posY = float(GetScreenHeight() / 2 - 50);
+	leftHitter.speed = 10;
+	rightHitter.speed = 10;
 
 	while (!WindowShouldClose())
 	{	
+
 		ball.posX +=  GetFrameTime() + ball.speedX;
 		ball.posY += GetFrameTime() + ball.speedY;
 		
-		if (IsKeyDown(KEY_W)) {
+
+		if (state == STARTED) {
+
+		if (IsKeyDown(KEY_W) && leftHitter.posY > 0) {
+			leftHitter.posY -= leftHitter.speed*1.1;
+		}
+		if (IsKeyDown(KEY_S) && leftHitter.posY < GetScreenHeight() - 100) {
+			leftHitter.posY += leftHitter.speed*1.1;
+		}
+		if (IsKeyDown(KEY_UP) && rightHitter.posY >0) {
+			rightHitter.posY -= rightHitter.speed* 1.1;
+		}
+		if (IsKeyDown(KEY_DOWN) && rightHitter.posY <= GetScreenHeight() - 100) {
+			rightHitter.posY += rightHitter.speed * 1.1;
+		}
+
+		if (ball.posY <= 0) {
+			ball.speedY *= -1;
+		}
+		if (ball.posY > GetScreenHeight()) {
+			ball.speedY *= -1;
+		}
+		if (CheckCollisionCircleRec(Vector2{ ball.posX,ball.posY }, ball.radius, leftHitter.getRect())) {
+			ball.speedX *= -1;
+		}
+		if (CheckCollisionCircleRec(Vector2{ ball.posX,ball.posY }, ball.radius, rightHitter.getRect())) {
+			ball.speedX *= -1;
+		}
+		if (ball.posX <= 0) {
+			state = PAUSED;
+			win = RIGHT;
+		}
+		if (ball.posX >= GetScreenWidth()) {
+			state = PAUSED;
+			win = LEFT;
+		}
 
 		}
-		if (IsKeyDown(KEY_S)) {
 
+		if (IsKeyPressed(KEY_SPACE)) {
+			state = STARTED;
+			win = NONE;
+			leftHitter.posX = 50.0f;
+			leftHitter.posY = float(GetScreenHeight() / 2 - 50);
+			rightHitter.posX = float(GetScreenWidth() - 50);
+			rightHitter.posY = float(GetScreenHeight() / 2 - 50);
+			leftHitter.speed = 10;
+			rightHitter.speed = 10;
+			ball.speedX = 5;
+			ball.speedY = 5;
+			ball.radius = 10;
+			//	ball.posX = GetScreenWidth() / 2;
+			//	ball.posY = GetScreenHeight() / 2;
+			ball.posX = 0.0f;
+			ball.posY = 0.0f;
 		}
-		if (IsKeyDown(KEY_UP)) {
 
-		}
-		if (IsKeyDown(KEY_DOWN)) {
-
-		}
 
 
 		BeginDrawing();
-
 			ClearBackground(BLACK);
+			if (win == LEFT) {
+				DrawText("Last win by Left", GetScreenWidth() / 2, GetScreenHeight() / 2 + 30, 30, DARKGREEN);
+			}
+			else if(win == RIGHT) {
+				DrawText("Last win by Right", GetScreenWidth() / 2, GetScreenHeight() / 2 + 30, 30, DARKGREEN);
+			}
+			if (state == PAUSED) {
+				DrawText("PRESS SPACE TO PLAY", GetScreenWidth() / 2, GetScreenHeight() / 2, 30, DARKGREEN);
+			}
+			else if(state == STARTED || state==NONE){
 
-			DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100,WHITE);
-			DrawRectangle(GetScreenWidth() - 50, GetScreenHeight()  / 2 - 50, 10, 100,WHITE);
-			ball.Draw();
-
+				leftHitter.Draw();
+				rightHitter.Draw();
+				ball.Draw();
+			}
 
 			DrawFPS(10,10);
 
